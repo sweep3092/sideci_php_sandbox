79ac489c28fb416b644177164ce1416f77cf468a
You Kobayashi <sweep.3092@gmail.com>
2016-03-04 18:11:07 +0900

    hogehogehoge

--- app/AppCache.php
+++ app/AppCache.php
@@ -4,7 +4,11 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
-
+  class HogeClass {
+    Class HogeHogeClass {
+    
+    }
+  }
 $ddddddddddddddddddddddddddddddddddddddddddddddddddd= '';
 
 }
